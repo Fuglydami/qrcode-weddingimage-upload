ea4e3412884dcda7d321ac330067803ce5990f8a
Damilare Oyedeji <damoye81@gmail.com>
2024-03-29 12:05:41 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <main className='bg-black min-h-screen grid place-content-center text-white'>
+      Hello world
+    </main>
+  );
+}
+
+export default App;
